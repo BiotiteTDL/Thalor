e9d47c5f6370a7f47c7adf02f386c74e6657086c
BiotiteTDL <riccardozagar@gmail.com>
2026-05-12 21:34:39 +0200

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -3,6 +3,6 @@
 // 2) Copia Project URL e anon public key qui sotto
 // 3) Lascia SERVICE_ROLE fuori dal sito: NON inserirla mai qui.
 window.THALOR_SUPABASE = {
-  url: 'https://dzbtbaoakqgaruuswgho.supabase.co/rest/v1/',
+  url: 'https://dzbtbaoakqgaruuswgho.supabase.co',
   anonKey: 'sb_publishable_enSm4eFWWuIqzAgx3o_KNw_Iosqqp-p'
 };
